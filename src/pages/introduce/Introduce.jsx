f6604714628ce1bc27977139a839ef0e0a4d4b4d
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-01-01 19:03:06 +0300

    File Structures
    File Structures Created

---
+++ src/pages/introduce/Introduce.jsx
@@ -0,0 +1,8 @@
+import './Introduce.css'
+
+const Introduce = () => {
+    return (
+        <div className='introduce'>Introduce</div>
+    )
+}
+export default Introduce
---
+++ src/pages/quiz/Quiz.jsx
@@ -0,0 +1,8 @@
+import './Quiz.css'
+
+const Quiz = () => {
+    return (
+        <div className='quiz'>Quiz</div>
+    )
+}
+export default Quiz
