764461594bd95abe1d96b88f1820ea11484ac37c
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-01-05 17:15:15 +0300

    dropdown
    dropdown events are prepared

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,15 +1,26 @@
 import Dropdown from '../../components/dropdown/Dropdown'
 import './Introduce.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Introduce = () => {
     const difficulty = ['easy', 'medium', 'hard']
+    const [difficultyChange, setDifficultyChange] = useState('')
+    const navigate = useNavigate()
+    const TOTAL_QUESTIONS = 10
+
+    const startQuiz = () => {
+        if (difficultyChange) {
+            navigate(`/quiz/${difficultyChange}/${TOTAL_QUESTIONS}`)
+        }
+    }
 
     return (
         <div className='introduce'>
             <div className='introduce-container'>
                 <img src="https://image.similarpng.com/very-thumbnail/2020/12/Lorem-ipsum-logo-isolated-clipart-PNG.png" alt="" />
-                <Dropdown />
-                <div className='introduce-btn'>Start Quiz</div>
+                <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange} />
+                <div onClick={startQuiz} className='introduce-btn'>Start Quiz</div>
             </div>
         </div>
     )
